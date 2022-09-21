41891c13af4abee3efa7e3f4ef107afb8233fbfb
hafeezarfi <hafeezarfi@gmail.com>
2022-09-21 21:59:00 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/hafeezarfi/shopingkart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hafeezarfi.shopingkart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopkart'),
+      ),
+    );
+  }
+}
